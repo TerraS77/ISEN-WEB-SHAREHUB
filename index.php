76a0fb0bd074334f6f319831d155a8b96b090016
MEGY MANON <manon.megy@gmail.com>
2021-12-30 17:01:44 +0100

    ajout de card boucle sans bdd

--- index.php
+++ index.php
@@ -44,7 +44,12 @@
 
 <div class="container">
 <div class="row row-cols-1 row-cols-md-6 g-2">
-  <div class="col">
+  <?php 
+  
+  for ( $i =0; $i<8;$i++){
+    ?>
+<html>
+<div class="col">
     <div class="card">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
@@ -53,35 +58,15 @@
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-</div>
-
+ </html>
+ <?php
+  }
+  
+  
+  
+  ?>
+  
+  
 </div>
 
 </body>
