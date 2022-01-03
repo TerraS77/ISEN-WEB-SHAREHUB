f469beb864223d36eb2f2a537f1c30706596de82
MEGY MANON <manon.megy@gmail.com>
2022-01-03 14:05:33 +0100

    page update

--- index.php
+++ index.php
@@ -1,74 +1,73 @@
 <html>
-    <head>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <link href="css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="css/index.css" rel="stylesheet"> 
-       
-    </head><body>
-<header >
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span><a class="navbar-brand" href="#"> Untilted</a>
-    </button>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span><a class="navbar-brand" href="#"> Untilted</a>
-    </button>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+
+<head>
+  <link href="css/index.css" rel="stylesheet">
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  
+
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span class="navbar-toggler-icon"></span><a class="navbar-brand" href="#"> Untilted</a>
+        </button>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+              <span class="navbar-toggler-icon"></span><a class="navbar-brand" href="#"> Untilted</a>
+            </button>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Share this hub</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Make your own hub</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+
+
+            </ul>
+            <div class="copyright">@Copyright</div>
+          </div>
+
+        </div>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Share this hub</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Make your own hub</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
-          </li>
-          
-          
-        </ul>
-        <div class="copyright">@Copyright</div>
+
+    </nav>
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title that wraps to a new line</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
       </div>
-      
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title that wraps to a new line</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-
-</nav>
-</header>
-
-<div class="container">
-<div class="row row-cols-1 row-cols-md-6 g-2">
-  <?php 
-  
-  for ( $i =0; $i<8;$i++){
-    ?>
-<html>
-<div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longerThis is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer</p>
-      </div>
-    </div>
-  </div>
- </html>
- <?php
-  }
-  
-  
-  
-  ?>
-  
-  
-</div>
-
 </body>
-</html>
 
+</html>
